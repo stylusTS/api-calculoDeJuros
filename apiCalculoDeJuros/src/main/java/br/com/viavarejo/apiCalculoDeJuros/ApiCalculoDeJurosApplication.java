122650205c74683712a1b5efefe1d179867f0c0d
Paulo <tecnostylus@gmail.com>
2019-07-26 15:53:07 -0300

    estruta do projeto corrigida

---
+++ apiCalculoDeJuros/src/main/java/br/com/viavarejo/apiCalculoDeJuros/ApiCalculoDeJurosApplication.java
@@ -0,0 +1,13 @@
+package br.com.viavarejo.apiCalculoDeJuros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCalculoDeJurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCalculoDeJurosApplication.class, args);
+	}
+
+}
